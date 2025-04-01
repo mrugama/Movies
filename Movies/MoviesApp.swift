42b573df1333fed97793d88196a12ad99813c3c4
Marlon Rugama <mrugama@gmail.com>
2025-04-01 11:59:34 -0400

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Marlon Rugama on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
